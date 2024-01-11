7a2040005e4d63f5645a91c4558976687d3a1d1f
Braisrg5 <46173493+Braisrg5@users.noreply.github.com>
2024-01-11 23:10:57 +0100

    Add files via upload
    PEP8 and useful_functions in resources folder
    
    Problems 11 and 12

--- solutions/problem_5.py
+++ solutions/problem_5.py
@@ -1,68 +1,44 @@
-'''2520 is the smallest number that can be divided by each of the numbers from
-1 to 10 without any reminder.
-What is the smallest positive number that is evenly divisible by all the 
-numbers from 1 to 20?
+'''2520 is the smallest number that can be divided by each of the
+numbers from 1 to 10 without any reminder.
+
+What is the smallest positive number that is evenly divisible by all
+the numbers from 1 to 20?
 '''
 
 
-from math import sqrt, floor
-
-def primeFactors(n, listFact = []):
-    '''Constructs a list of the factors of a number, from smallest to largest. 
-    Works in a similar manner to the previous function, but calls itself after
-    adding each factor, and at the same time divides the number by them
-    '''
-    end = floor(sqrt(n))
-    if n == 1:
-        return listFact
-    elif n % 2 == 0:
-        return primeFactors(n//2, listFact + [2])
-    elif n % 3 == 0:
-        return primeFactors(n//3, listFact + [3])
-    for j in range(6, end+2, 6):
-        p1, p2 = j-1, j+1
-        if n % p1 == 0:
-            return primeFactors(n//p1, listFact + [p1])
-        elif n % p2 == 0:
-            return primeFactors(n//p2, listFact + [p2])
-    return listFact + [n]
-
-def counts(l):
-    '''Counts the number of times an element appears on a list.
-    '''
-    counts = dict()
-    for i in l:
-        counts[i] = counts.get(i, 0) + 1
-    return counts
+from resources.useful_functions import prime_factors, counts
 
 
-def factorsToNumber(factors):
-    '''Translates a dictionary of prime factors to a number.
+def factors_to_number(factors):
+    '''Translates a dictionary of prime factors and exponents to a
+    number.
     '''
     n = 1
     for key, value in factors.items():
         n *= key**value
     return n
 
+
 def lcm(numbers):
-    '''Finds the least common multiple of a list of numbers, following the
-    method of multiplying the prime factors to the highest exponent.
-    A method using the Euclidean algorithm is in the back of my mind.
+    '''Finds the least common multiple of a list of numbers, following
+    the method of multiplying the prime factors to the highest
+    exponent. A method using the Euclidean algorithm is in the back of
+    my mind.
     '''
-    factorsList = [counts(primeFactors(n)) for n in numbers]
-    newFactors = dict()
-    for factors in factorsList:
+    factors_list = [counts(prime_factors(n)) for n in numbers]
+    new_factors = dict()
+    for factors in factors_list:
         for key, value in factors.items():
-            if key in newFactors.keys():
-                if value > newFactors[key]:
-                    newFactors[key] = value
+            if key in new_factors.keys():
+                if value > new_factors[key]:
+                    new_factors[key] = value
             else:
-                newFactors[key] = value
-    return factorsToNumber(newFactors)
+                new_factors[key] = value
+    return factors_to_number(new_factors)
 
 
 if __name__ == '__main__':
-    tenFirst = list(range(1,11))
-    print(lcm(tenFirst)) # 2520
-    twentyFirst = list(range(1,21))
-    print(lcm(twentyFirst)) # 232792560
+    ten_first = list(range(1, 11))
+    print(lcm(ten_first))  # 2520
+    twenty_first = list(range(1, 21))
+    print(lcm(twenty_first))  # 232792560, 0.0s
---
+++ solutions/resources/useful_functions.py
@@ -0,0 +1,52 @@
+from math import floor, sqrt
+
+
+def is_prime(n):
+    """Checks if n is a prime number by dividing it by 2 and 3 and then
+    checking the numbers of the form 6*j + 1 and 6*j - 1, where j is an
+    integer, and up to the square root of n. This are the only numbers
+    of the form 6*j + k than can be prime, and thus we skip many
+    numbers and reduce computation time.
+    """
+    end = floor(sqrt(n))
+    if n == 1:
+        return False
+    elif n in (2, 3):
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    # end+2 to ensure that 6*j+1 is included
+    for j in range(6, end + 2, 6):
+        if n % (j - 1) == 0 or n % (j + 1) == 0:
+            return False
+    return True
+
+
+def prime_factors(n, listFact=[]):
+    """Constructs a list of the factors of a number n, from smallest to
+    largest. Works in a similar manner to the previous function, but
+    calls itself after adding each factor, and at the same time divides
+    the number by them
+    """
+    end = floor(sqrt(n))
+    if n == 1:
+        return listFact
+    elif n % 2 == 0:
+        return prime_factors(n // 2, listFact + [2])
+    elif n % 3 == 0:
+        return prime_factors(n // 3, listFact + [3])
+    for j in range(6, end + 2, 6):
+        p1, p2 = j - 1, j + 1
+        if n % p1 == 0:
+            return prime_factors(n // p1, listFact + [p1])
+        elif n % p2 == 0:
+            return prime_factors(n // p2, listFact + [p2])
+    return listFact + [n]
+
+
+def counts(ls):
+    """Counts the number of times an element appears on a list."""
+    counts = dict()
+    for i in ls:
+        counts[i] = counts.get(i, 0) + 1
+    return counts
