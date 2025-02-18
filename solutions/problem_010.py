b1a700eb8e5d065e5bd9e51c002c37468f7bebe3
Brais <braisrg5@gmail.com>
2025-02-18 08:15:13 +0100

    Add files via upload

--- solutions/problem_010.py
+++ solutions/problem_010.py
@@ -9,4 +9,4 @@ from resources.useful_functions import sieve_Eratosthenes
 
 if __name__ == "__main__":
     print(sum(sieve_Eratosthenes(10)))  # 17
-    print(sum(sieve_Eratosthenes(2000000)))  # 142913828922, 0.43s
+    print(sum(sieve_Eratosthenes(2000000)))  # 142913828922, 0.18s
