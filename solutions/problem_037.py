e4118778f4fc6d360df01f76a4ee35dcfe2c3adb
Brais <braisrg5@gmail.com>
2025-02-24 01:42:45 +0100

    Comments up to problem 38

--- solutions/problem_037.py
+++ solutions/problem_037.py
@@ -15,6 +15,15 @@ from resources.useful_functions import is_prime, digits_odd, sieve_Eratosthenes
 from math import floor, log10
 
 
+def mod_digits_odd(n):
+    '''Returns True if all the digits of a number n are odd or all are odd and
+    the first digit is 2.'''
+    n = str(n)
+    all_odd = all(int(i) % 2 for i in n)
+    first2_all_odd = all(int(i) % 2 for i in n[1:]) and n[0] == '2'
+    return all_odd or first2_all_odd
+
+
 def remove_digits(prime):
     '''Returns a list of the truncations from left to right and from
     right to left of a prime.'''
@@ -45,7 +54,7 @@ def sum_truncatable_primes(bound):
     '''Sums all the truncatable primes.
     Knowing the last truncatable prime is 739397, a bigger number can
     be the bound.'''
-    trunc_primes = [23]
+    trunc_primes = []
     for k in range(12, bound, 6):
         p1 = k-1
         p2 = k+1
@@ -80,21 +89,27 @@ def sum_truncatable_primes_v2(bound):
 
 def check_truncatable_v2(p, primes_set):
     '''Returns whether a prime is truncatable.'''
-    digits = int(log10(p))
+    num_digits = int(log10(p))
+
     # Right to left
     p_check = p
     while p_check > 10:
+        # Integer division by 10 returns the number without the last digit
         p_check //= 10
+        # If any of the truncations fail, return False
         if p_check not in primes_set:
             return False
 
     # Left to right
     p_check = p
     while p_check > 10:
-        p_check %= 10**digits
-        digits -= 1
+        # Modulo by powers of 10 gives the number without the first digit
+        p_check %= 10**num_digits
+        num_digits -= 1
+        # If any of the truncations fail, return False
         if p_check not in primes_set:
             return False
+    # If all truncations are prime, return True
     return True
 
 
@@ -102,14 +117,17 @@ def sum_truncatable_primes_v3(bound):
     '''Sums all the truncatable primes.'''
     primes = sieve_Eratosthenes(bound)
     primes_set = set(primes)
+    # Exclude 2, 3, 5, 7
     primes = primes[4:]
 
-    trunc_primes = [23]
+    # Initialize the list of truncatable primes
+    trunc_primes = []
     for p in primes:
-        if digits_odd(p) and check_truncatable_v2(p, primes_set):
+        # If any of the digits is even (except if the first is 2), skip prime
+        if mod_digits_odd(p) and check_truncatable_v2(p, primes_set):
             trunc_primes.append(p)
+        # We've found all the truncatable primes
         if len(trunc_primes) == 11:
-            print(trunc_primes)
             return sum(trunc_primes)
     # Bound too small
     return -1
--- solutions/problem_038.py
+++ solutions/problem_038.py
@@ -81,9 +81,9 @@ def pandigital_multiples_2():
 
 # No need to check for n = 3, for x = 329 the number is less than 918273645
 '''def pandigital_multiples_3():
-    '''Finds all 1 to 9 pandigital 9-digit number that can be formed as
+    """Finds all 1 to 9 pandigital 9-digit number that can be formed as
     the concatenated products of an integer with n = 3.
-    '''
+    """
     pandigital_multiples = {}
     for x in range(123, 330):
         concatenate = str(x) + str(2*x) + str(3*x)
@@ -97,3 +97,51 @@ if __name__ == '__main__':
     # print(pandigital_multiples_3())
     # print(max(pandigital_multiples_3().values()))  # 327654981
     print(max(pandigital_multiples_2().values()))  # 932718654, 0.0007s
+
+
+'''
+#-------#
+# Notes #
+#-------#
+
+(1*)
+For n = 2, since we need to obtain 9 digits, the digits of x and 2x must sum to
+9. Since x < 2x and we can only increase by one digit each time we multiply, we
+have the following conditions:
+            4 digits: x * 1 ∈ [1234, 9876]
+            5 digits: x * 2 ∈ [12345, 98765] => x ∈ [6173, 49382]
+            The intersection of the two intervals gives us x ∈ [6173, 9876]
+
+For n = 3, we need:
+            3 digits: x * 1 ∈ [123, 987]
+            3 digits: x * 2 ∈ [123, 987] => x ∈ [62, 493]
+            3 digits: x * 3 ∈ [123, 987] => x ∈ [41, 329]
+            The intersection of the three intervals gives us x ∈ [123, 329]
+            The concatenated product with 329 would be less than 918273645.
+
+For n = 4, we need:
+            2 digits: x * 1 ∈ [12, 98]
+            2 digits: x * 2 ∈ [12, 98] => x ∈ [6, 49]
+            2 digits: x * 3 ∈ [12, 98] => x ∈ [4, 32]
+            3 digits: x * 4 ∈ [123, 987] => x ∈ [31, 246]
+            The intersection of the four intervals gives us x ∈ [31, 32]
+            31*1 = 31, 31*2 = 62, 31*3 = 93, 31*4 = 124, not pandigital.
+            32*1 = 32, 32*2 = 64, 32*3 = 96, 32*4 = 128, not pandigital.
+
+For n = 5, we need:
+            1 digit: x * 1 ∈ [1, 9]
+            2 digits: x * 2 ∈ [12, 98] => x ∈ [6, 49]
+            2 digits: x * 3 ∈ [12, 98] => x ∈ [4, 32]
+            2 digits: x * 4 ∈ [12, 98] => x ∈ [3, 24]
+            2 digits: x * 5 ∈ [12, 98] => x ∈ [3, 19]
+            The intersection of the five intervals gives us x ∈ [6, 9]
+            6*1 = 6, 6*2 = 12, 6*3 = 18, not pandigital.
+            7*1 = 7, 7*2 = 14, 7*3 = 21, not pandigital.
+            8*1 = 8, 8*2 = 16, 8*3 = 24, 8*4 = 32, not pandigital.
+            For 9, we already know it is pandigital.
+
+For n > 5, it is impossible.
+
+The algorithm will iterate over the case for n = 2 and return the largest
+pandigital number found.
+'''
